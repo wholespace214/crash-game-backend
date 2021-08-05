71f857b10eec7120ff645a654f21c3accfb2162e
wholespace214 <wholespace.214@gmail.com>
2021-08-05 12:40:41 +0200

    configure github secrets for the github actions

--- .github/scripts/generate-spec.js
+++ .github/scripts/generate-spec.js
@@ -2,4 +2,13 @@ const path = require("path");
 const fs = require("fs");
 
 let yml = fs.readFileSync(path.resolve(__dirname, `spec-${process.env.stage}.yml`)).toString();
-console.log(yml.replace("$VERSION", process.env.version));
+console.log(
+    yml
+    .replace("$VERSION", process.env.version)
+    .replace("$JWT", process.env.JWT_KEY)
+    .replace("$TWILIO_ACC_SID", process.env.TWILIO_ACC_SID)
+    .replace("$TWILIO_AUTH_TOKEN", process.env.TWILIO_AUTH_TOKEN)
+    .replace("$TWILIO_SID", process.env.TWILIO_SID)
+    .replace("$POSTGRES_PASSWORD", process.env.POSTGRES_PASSWORD)
+    .replace("$GMAIL_PASSWORD", process.env.GMAIL_PASSWORD)
+    );
