a28b272d349bac511d4d363bd9d97ebfa03c767b
wholespace214 <wholespace.214@gmail.com>
2021-08-10 19:40:01 +0200

    MVP2-6 - Allow non-logged in user to fetch events and latest chat messages

--- services/chat-message-service.js
+++ services/chat-message-service.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 // Import ChatMessage model
 const ChatMessage = require("../models/ChatMessage");
 
@@ -6,7 +7,44 @@ exports.getChatMessagesByEvent = async (eventId) => {
 };
 
 exports.getNewestChatMessagesByEvent = async (eventId, limit=100, skip=0) => {
-    return ChatMessage.find({eventId}, null, {sort: {date: -1}, limit, skip});
+    return ChatMessage.aggregate([
+        {
+            $match: { eventId: mongoose.Types.ObjectId(eventId) }
+        },
+        { $sort: { date: -1 }},
+        { $limit : limit },
+        { $skip : skip },
+        {
+          $lookup: {
+            localField: "userId",
+            from: "users",
+            foreignField: "_id",
+            as: "user"
+          }
+        },
+        {
+            $replaceRoot: { 
+                newRoot: { 
+                    $mergeObjects: [ 
+                        { 
+                            $arrayElemAt: [ "$user", 0 ] 
+                        }, 
+                        "$$ROOT" 
+                    ] 
+                } 
+            }
+        },
+        { $project: 
+            { 
+                userId: 1, 
+                eventId: 1, 
+                type: 1, 
+                message: 1, 
+                date: 1, 
+                name: 1 
+            } 
+        }
+      ])
 };
 
 exports.createChatMessage = async (data) => {
