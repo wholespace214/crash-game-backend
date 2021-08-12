575b3b27e3375e6a4f790c722e9cc1eb095a84f4
wholespace214 <wholespace.214@gmail.com>
2021-08-12 11:40:36 +0200

    - expose bet outcome calculation routes (buy/sell) public

--- routes/users/events-routes.js
+++ routes/users/events-routes.js
@@ -2,6 +2,7 @@
 const router = require("express").Router();
 const { check } = require("express-validator");
 const eventController = require("../../controllers/events-controller");
+const betController = require("../../controllers/bets-controller");
 
 router.get(
     "/list",
@@ -14,6 +15,22 @@ router.get(
         check("id").notEmpty()
     ],
     eventController.getChatMessagesByEventId
+);
+
+router.post(
+    "/bet/:id/outcomes/buy",
+    [
+        check("amount").isNumeric()
+    ],
+    betController.calculateBuyOutcome
+);
+
+router.post(
+    "/bet/:id/outcomes/sell",
+    [
+        check("amount").isNumeric()
+    ],
+    betController.calculateSellOutcome
 );
 
 module.exports = router;
