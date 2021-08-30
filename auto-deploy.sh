d00bd7d34dbbf4213b63f7d144557828facf7afc
wholespace214 <wholespace.214@gmail.com>
2021-08-30 13:59:45 +0200

    Remove unnecessary files

--- auto-deploy.sh
+++
@@ -1,3 +0,0 @@
-docker login -u ${DIGITALOCEAN_ACCESS_TOKEN} -p ${DIGITALOCEAN_ACCESS_TOKEN} registry.digitalocean.com
-docker build . -t registry.digitalocean.com/wallfair/backend -f Dockerfile.node
-docker push registry.digitalocean.com/wallfair/backend
