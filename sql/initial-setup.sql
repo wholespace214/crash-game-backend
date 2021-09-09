f40c9bcde3401bae2d0e6b82f552b2d5b2d1f16f
wholespace214 <wholespace.214@gmail.com>
2021-09-09 23:39:48 +0200

    add indexes to startup script

--- sql/initial-setup.sql
+++ sql/initial-setup.sql
@@ -3,3 +3,8 @@ CREATE TABLE IF NOT EXISTS token_balances (owner varchar(255) not null, balance 
 CREATE TABLE IF NOT EXISTS bet_reports (bet_id varchar(255) not null PRIMARY KEY, reporter varchar(255) not null, outcome smallint not null, report_timestamp timestamp not null);
 CREATE TABLE IF NOT EXISTS amm_interactions (ID SERIAL PRIMARY KEY, buyer varchar(255) NOT NULL, bet varchar(255) NOT NULL, outcome smallint NOT NULL, direction varchar(10) NOT NULL, investmentAmount bigint NOT NULL, feeAmount bigint NOT NULL, outcomeTokensBought bigint NOT NULL, trx_timestamp timestamp NOT NULL);
 CREATE TABLE IF NOT EXISTS casino_trades (ID SERIAL PRIMARY KEY, userId varchar(255) NOT NULL, crashFactor decimal NOT NULL, stakedAmount bigint NOT NULL, state smallint NOT NULL, gameId varchar(255), created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);
+CREATE INDEX buyer_idx ON amm_interactions (buyer);
+CREATE INDEX bet_idx ON amm_interactions (bet);
+CREATE INDEX owner_idx ON token_balances (owner);
+CREATE INDEX bet_rep_idx ON bet_reports (bet_id);
+CREATE INDEX sende_receiver_idx ON token_transactions (sender, receiver);
