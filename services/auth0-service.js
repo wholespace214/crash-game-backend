962a09234e5a4b377adf61109299cfb9c5f26713
wholespace214 <wholespace.214@gmail.com>
2021-09-28 16:29:46 +0200

    fixed backend

--- services/auth0-service.js
+++ services/auth0-service.js
@@ -3,8 +3,18 @@ const jwt = require('express-jwt');
 const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
 
-const { AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, CLIENT_URL } = process.env
+const {
+  AUTH0_DOMAIN,
+  AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET,
+  AUTH0_AUDIENCE,
+  CLIENT_URL
+} = process.env
 
+if (!AUTH0_DOMAIN) throw new Error("AUTH0_DOMAIN isn't defined")
+if (!AUTH0_CLIENT_ID) throw new Error("AUTH0_CLIENT_ID isn't defined")
+if (!AUTH0_CLIENT_SECRET) throw new Error("AUTH0_CLIENT_SECRET isn't defined")
+if (!AUTH0_AUDIENCE) throw new Error("AUTH0_AUDIENCE isn't defined")
 /*
  * This is completely intended for internal use and not to be exposed via route or similar.
  */
@@ -16,6 +26,7 @@ const options = {
   domain: AUTH0_DOMAIN,
   clientId: AUTH0_CLIENT_ID,
   clientSecret: AUTH0_CLIENT_SECRET,
+  audience: AUTH0_AUDIENCE
 };
 
 const managementClient = new ManagementClient(options);
@@ -37,8 +48,11 @@ exports.deleteUser = managementClient.deleteUser;
 /**
  * Authorization middleware. When used, the
  * Access Token must exist and be verified against
- * the Auth0 JSON Web Key Set
+ * the Auth0 JSON Web Key Set.
+ * The middleware doesn't check if the token has the sufficient scope to access
+ * the requested resources!
  */
+
 exports.checkJwt = jwt({
   // Dynamically provide a signing key
   // based on the kid in the header and
@@ -51,8 +65,10 @@ exports.checkJwt = jwt({
   }),
 
   // Validate the audience and the issuer.
-  audience: 'YOUR_API_IDENTIFIER',
-  issuer: [`https://${AUTH0_DOMAIN}`],
-  algorithms: ['RS256']
+  aud: AUTH0_AUDIENCE,
+  issuer: [`https://${AUTH0_DOMAIN}/`],
+  algorithms: ['RS256'],
 });
+
+// TODO Let's see if we want the work with scopes here
 exports.checkScopes = jwtAuthz(['read:messages']);
