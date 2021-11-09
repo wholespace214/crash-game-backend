81a6a1bf0f8add30fed5f62a6c2b711699b7120a
wholespace214 <wholespace.214@gmail.com>
2021-11-09 15:14:42 +0000

    add german and russian profanity filter

--- services/chat-message-service.test.js
+++ services/chat-message-service.test.js
@@ -8,9 +8,23 @@ describe('profanity filter', () => {
     expect(result.message).to.equal(message);
   });
 
-  it('should remove non-profanities', async () => {
+  it('should remove english profanities', async () => {
     const message = 'a shit chat message';
     const expected = `a ${profanityReplacement.repeat(4)} chat message`;
+    const result = await profanityFilter({ message });
+    expect(result.message).to.equal(expected);
+  });
+
+  it('should remove german profanities', async () => {
+    const message = 'a Scheisse chat message';
+    const expected = `a ${profanityReplacement.repeat(8)} chat message`;
+    const result = await profanityFilter({ message });
+    expect(result.message).to.equal(expected);
+  });
+
+  it('should remove russian profanities', async () => {
+    const message = 'a дерьмо chat message';
+    const expected = `************* chat message`;
     const result = await profanityFilter({ message });
     expect(result.message).to.equal(expected);
   });
