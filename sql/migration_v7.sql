219bde7091ecee0e64c164ca4273417948716602
wholespace214 <wholespace.214@gmail.com>
2021-11-26 17:06:41 +0100

    1. add casino_fairness table, for saving current client / server seeds

---
+++ sql/migration_v7.sql
@@ -0,0 +1,3 @@
+BEGIN;
+CREATE TABLE IF NOT EXISTS casino_fairness (ID SERIAL PRIMARY KEY, userId varchar(255) NOT NULL, gameId varchar(255), serverSeed varchar(255), clientSeed varchar(255), nonce INT, currentHashLine INT, created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);
+COMMIT;
