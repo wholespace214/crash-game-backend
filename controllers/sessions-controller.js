c19327d92dd29ddcd754ee90a09066a192175f94
wholespace214 <wholespace.214@gmail.com>
2021-12-01 01:42:22 +0700

    fix: for debugging

--- controllers/sessions-controller.js
+++ controllers/sessions-controller.js
@@ -71,7 +71,8 @@ module.exports = {
       });
 
       const account = new TransactionManager().account;
-      await account.createUser(wFairUserId);
+      await account.createUser(wFairUserId)
+        .catch((error)=> next(new ErrorHandler(500, error)));
 
       // TODO: When there's time, delete Auth0 user if WFAIR creation fails
 
