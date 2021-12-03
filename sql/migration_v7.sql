47f2a668bbb7339226c7d8d2cd69e86308db746f
wholespace214 <wholespace.214@gmail.com>
2021-12-03 19:10:01 +0100

    - add multiplication for old values in v7 migration

--- sql/migration_v7.sql
+++ sql/migration_v7.sql
@@ -9,4 +9,8 @@ ALTER TABLE casino_matches ALTER COLUMN amountRewardedSum TYPE numeric;
 
 ALTER TABLE casino_trades ALTER COLUMN stakedAmount TYPE numeric;
 
+-- UPDATE casino_matches SET amountRewardedSum=amountRewardedSum*100000000000000;
+-- UPDATE casino_matches SET amountInvestedSum=amountInvestedSum*100000000000000;
+-- UPDATE casino_trades SET stakedAmount=stakedAmount*100000000000000;
+
 COMMIT;
