05bcf565d407603a4640796cd66bbf453b9cada1
wholespace214 <wholespace.214@gmail.com>
2021-12-14 11:39:29 +0100

    twitch login

--- services/auth-service.js
+++ services/auth-service.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { getFacebookUserData } = require('../util/facebook.oauth');
 const { getGoogleUserData } = require('../util/google.oauth');
+const { getTwitchUserData } = require('../util/twitch.oauth');
 
 exports.generateJwt = async (user) => jwt.sign({ userId: user.id, phone: user.phone }, process.env.JWT_KEY, { expiresIn: '48h' });
 
@@ -8,6 +9,7 @@ exports.getUserDataForProvider = async (provider, context) => {
   const dataGetter = {
     google: getGoogleUserData,
     facebook: getFacebookUserData,
+    twitch: getTwitchUserData,
   }[provider];
 
   if (!dataGetter) {
