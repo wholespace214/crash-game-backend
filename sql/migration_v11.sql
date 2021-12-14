bc13a1c4fd8a4be732bdb4bc88802d291ac5d85e
wholespace214 <wholespace.214@gmail.com>
2021-12-14 14:30:12 +0100

    - extend casino_rewards table

--- sql/migration_v11.sql
+++ sql/migration_v11.sql
@@ -1,3 +1,3 @@
 BEGIN;
-CREATE TABLE IF NOT EXISTS casino_rewards (ID SERIAL PRIMARY KEY, userId varchar(255) NOT NULL, gameId varchar(255), type varchar(255), amount NUMERIC, created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE IF NOT EXISTS casino_rewards (ID SERIAL PRIMARY KEY, userId varchar(255) NOT NULL, refId varchar(255), tradeId int, gameId varchar(255), type varchar(255), amount NUMERIC, created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);
 COMMIT;
