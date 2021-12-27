6c97811334902b3cabd27d660218bf5e2c813925
wholespace214 <wholespace.214@gmail.com>
2021-12-27 14:18:05 +0100

    - handle request logs

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const { handleError } = require('./util/error-handler');
 const { initDb } = require('@wallfair.io/trading-engine');
 // const { initDatabase } = require('@wallfair.io/wallfair-casino');
 
+const { requestLoggerHandler } = require('./services/requestLogger');
+
 let mongoURL = process.env.DB_CONNECTION;
 
 /**
@@ -112,6 +114,9 @@ async function main() {
   server.use(adminService.getLoginPath(), adminService.getRouter());
   server.use(express.json({ limit: '5mb' }));
   server.use(express.urlencoded({ limit: '5mb', extended: true }));
+
+  // request logger handler
+  server.use(requestLoggerHandler)
 
   // Home Route
   server.get('/', (req, res) => {
---
+++ services/requestLogger.js
@@ -0,0 +1,78 @@
+const models = require('@wallfair.io/wallfair-commons').models;
+
+const API_TYPE = 'backend';
+const DATA_SENSITIVE_ROUTES = [
+  '/auth/login'
+];
+
+const getRealIp = (req) => {
+  return req.headers['x-forwarded-for'] || req.headers['cf-ipcountry'] || req.socket.remoteAddress || req.connection.remoteAddress;
+}
+
+const getUsefullHeaders = (req) => {
+  const output = {};
+  const headers = req.headers;
+  const list = [
+    'content-type',
+    'user-agent',
+    'referer'
+  ];
+
+  for (const header in headers) {
+    if (list.indexOf(header) > -1) {
+      output[header] = headers[header];
+    }
+  }
+
+  return output;
+}
+
+
+/**
+ * Get body, excluding some data-sensitive routes
+ * @param req
+ * @returns {{}}
+ */
+const getBody = (req) => {
+  const path = req.path;
+  const list = DATA_SENSITIVE_ROUTES;
+
+  let output = {};
+
+  for (const index in list) {
+    if (path.indexOf(list[index]) === -1) {
+      output = req.body;
+    }
+  }
+
+  return output;
+}
+
+const requestLoggerHandler = async (req, res, next) => {
+  try {
+    const entry = {
+      api_type: API_TYPE,
+      userId: req._userId,
+      ip: getRealIp(req),
+      method: req.method,
+      path: req.path,
+      query: req.query,
+      headers: getUsefullHeaders(req),
+      body: getBody(req),
+      statusCode: res.statusCode
+    }
+
+    await models.ApiLogs.create(entry);
+    console.log('entry', entry);
+
+  } catch (error) {
+    console.error(`${new Date()} [requestLoggerHandler] error`, error);
+  } finally {
+    next();
+  }
+};
+
+
+module.exports = {
+  requestLoggerHandler
+}
