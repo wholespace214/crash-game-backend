9c00ec025a44f9bd99814df3325fee8c3eef9ca9
wholespace214 <wholespace.214@gmail.com>
2022-01-26 22:46:54 +0100

    - moonpay, encode email before hmac signature

--- services/moonpay-service.js
+++ services/moonpay-service.js
@@ -3,7 +3,15 @@ const crypto = require('crypto')
 const { MOONPAY_BASE_URL, MOONPAY_API_KEY, MOONPAY_API_SECRET, MOONPAY_CURRENCY_CODE, ONRAMP_WEBHOOK_WALLET } = process.env;
 
 exports.generateUrl = (userId, email, amount, currency) => {
-  const baseUrl = `${MOONPAY_BASE_URL}?apiKey=${MOONPAY_API_KEY}&externalCustomerId=${userId}&email=${email}&baseCurrencyCode=${currency.toLowerCase()}&currencyCode=${MOONPAY_CURRENCY_CODE}&baseCurrencyAmount=${amount}&walletAddress=${ONRAMP_WEBHOOK_WALLET}&colorCode=%23ffd401`;
+  const baseUrl = MOONPAY_BASE_URL +
+    `?apiKey=${MOONPAY_API_KEY}` +
+    `&externalCustomerId=${userId}` +
+    `&email=${encodeURIComponent(email)}` +
+    `&baseCurrencyCode=${currency.toLowerCase()}` +
+    `&currencyCode=${MOONPAY_CURRENCY_CODE}` +
+    `&baseCurrencyAmount=${amount}` +
+    `&walletAddress=${ONRAMP_WEBHOOK_WALLET}` +
+    `&colorCode=%23ffd401`;
 
   const signature = crypto
     .createHmac('sha256', MOONPAY_API_SECRET)
