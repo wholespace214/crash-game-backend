28795f4ce520038038fdeb5ec7ffac4d897dafb2
wholespace214 <wholespace.214@gmail.com>
2022-03-15 17:46:01 +0100

    Return admin flag on web3 login

--- controllers/sessions-controller.js
+++ controllers/sessions-controller.js
@@ -354,6 +354,7 @@ module.exports = {
       return res.status(200).json({
         session: token,
         userId: user.id,
+        admin: user.admin,
         shouldAcceptToS: hasAcceptedLatestConsent(user),
       });
     } catch (e) {
